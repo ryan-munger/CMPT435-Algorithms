2c0f7f2750f2a4d64328285978efa0b067c841e1
Ryan <ryanmunger10@gmail.com>
2024-09-06 19:23:46 -0400

    Read in magicItems

--- Assignment1/assignment1.cpp
+++ Assignment1/assignment1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class Node {
@@ -12,6 +14,20 @@ class Node {
         next = nullptr; // next is not ready when making current
     }
 };
+
+vector<string> getMagicItems(const string&filename) {
+    vector<string> magicItems; 
+    ifstream file("magicItems.txt"); //input file stream
+    if (!file) {
+        cerr << "File opening failed." << endl;
+    }
+    string line;
+    while (getline(file, line)) {
+        magicItems.push_back(line); // add line to vector
+    }
+    file.close();
+    return magicItems;
+}
 
 int main() {
     // Testing Node class
@@ -31,5 +47,10 @@ int main() {
     delete firstNode;
     delete secondNode;
 
+    vector<string> magicItems = getMagicItems("magicItems.txt");
+    for (const auto& ln : magicItems) {
+        std::cout << ln << std::endl;
+    }
+    
     return 0;
 };
